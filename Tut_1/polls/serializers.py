1d9c51b29ce9eb90cbbfa9a3431b5143b1e9f79f
Denys Kotsiuba <denis.kotsiuba@gmail.com>
2020-04-16 23:53:29 +0300

    fixed serializer

--- Tut_1/polls/serializers.py
+++ Tut_1/polls/serializers.py
@@ -2,13 +2,12 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Question, Choice
 
-class QuestionSerializer(serializers.HyperlinkedModelSerializer):
+class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Question
         fields = ['id', 'question_text', 'pub_date']
 
-class ChoiceSerializer(serializers.HyperlinkedModelSerializer):
+class ChoiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Choice
-        #fields = ['id', 'question', 'choice_text', 'vote']
-        fields = ['id', 'choice_text', 'vote']
+        fields = ['id', 'question', 'choice_text', 'vote']
